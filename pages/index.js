903196235a00a63fc84c8948d956d33dd1b95969
siripods <siripods@gmail.com>
2023-06-07 14:21:53 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+
+function IndexPage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <ul>
+                <li>
+                    <Link href="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link href="/clients">Clients</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default IndexPage;
